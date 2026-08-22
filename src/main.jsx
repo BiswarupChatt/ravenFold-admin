6364096b17b652c50d7dd2aae4e87ec30821305b
R Q <chatterjeebiswarup61@gmail.com>
2026-08-23 00:11:50 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider as JotaiProvider } from 'jotai'
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <JotaiProvider>
         <App />
       </JotaiProvider>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
